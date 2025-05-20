4b83266cc9bf5272e7954471f589079a09a60c65
mlr58 <166216437+mlr58@users.noreply.github.com>
2025-05-20 10:34:18 +0200

    Update plot_maker.py

--- plot_maker.py
+++ plot_maker.py
@@ -4,14 +4,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
+from os import listdir
+from os.path import isfile, join
 
 
 def main():
     st.set_page_config(layout='centered')
 
+    st.title('Data Cleaning App')
+
+    file = st.file_uploader('Data to clean', type=['csv', 'txt'], key='file_uploader')
+
+    
+    text = file.read().decode('utf-8')
+
+    text = text.replace('-', '')
+    text = text.replace('\n\n', '\n')
+    text.strip('\n')
+    text.strip()
+    elements = text.split('\n\n ')
+    
+
+    for element in elements:
+        index = element.split('\n')[0]
+        if index.strip() != '':
+            st.download_button(
+                    label=f'Download {index}',
+                    data=element,
+                    file_name=f'{index}.csv',
+                    mime='text/csv'
+                )
+
+    
+
+
+
+
+
+
+
+
+
     st.title('Data Plotting App')
 
-    file = st.file_uploader('Upload a CSV file', type='csv', key='file_uploader')
+    file = st.file_uploader('Data to plot', type='csv', key='uploader')
 
     x_label = st.text_input('X Label', '')
     y_label = st.text_input('Y Label', '')
@@ -38,6 +74,8 @@ def main():
         st.pyplot(plt)
     except:
         pass
+
+
 
 
 
